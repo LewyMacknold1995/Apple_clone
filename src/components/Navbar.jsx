7dfb1f92999e6634c82884fd396bb76416bfb07a
Lewy Macknold <lewymacknoldseo@gmail.com>
2024-07-13 14:52:13 +0100

    created layout of navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { appleImg, bagImg, searchImg } from '../utils';
 
-const Navbar = () => {
+const Navbar= () => {
   return (
-    <div>Navbar</div>
+    <header>
+        <nav>
+            <img src={appleImg} alt='Apple' width={14} height={18} />
+
+            <div>
+                {['Phones', 'Macbooks', 'Tablets'].map((nav) => (
+                    <div key={nav}>
+                        {nav}
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                <img src={searchImg} alt='search' width={18} height={18}/>
+                <img src={bagImg} alt='bag' width={18} height={18}/>
+            </div>
+            
+        </nav>
+
+    </header>
   )
 }
 
