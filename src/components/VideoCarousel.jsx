2502c432cdce1af93bca08a784a86a8fc2059561
Lewy Macknold <lewymacknoldseo@gmail.com>
2024-07-15 21:28:46 +0100

    made highlights contents

---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const VideoCarousel = () => {
+  return (
+    <>
+        <div classname> 
+        
+        </div>    
+    </>
+  )
+}
+
+export default VideoCarousel
